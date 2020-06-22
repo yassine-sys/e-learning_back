369734c88131647bd381314b5645bda897fa4dfd
tesnim elfidha <tesnim.el.fidha@ced.group>
2020-06-22 16:28:53 +0100

    repo

--- CEDAcademyAPI/DataAccess/IRepositories/IExamRepository.cs
+++ CEDAcademyAPI/DataAccess/IRepositories/IExamRepository.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,12 @@ using System.Threading.Tasks;
 
 namespace DataAccess.IRepositories
 {
-    interface IExamRepository
+    interface IExamRepository<Exam> where Exam : class
     {
+        IEnumerable<Exam> GetExams();
+        Exam GetExamByID(int ExamID);
+        void InsertExam(Exam exam);
+        void DeleteExam(int ExamID);
+        void UpdateExam(Exam exam);
     }
 }
