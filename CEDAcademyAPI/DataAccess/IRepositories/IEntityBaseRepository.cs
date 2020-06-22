52cb27b8288aecd90f4d64dd96d1004f409429d7
yassine-sys <61196442+yassine-sys@users.noreply.github.com>
2020-06-22 11:18:12 +0200

    sss

---
+++ CEDAcademyAPI/DataAccess/IRepositories/IEntityBaseRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.IRepositories
+{
+    public interface IEntityBaseRepository<TEntity> where TEntity : class
+    {
+     
+        void Delete(object id);
+        IEnumerable<TEntity> Get(Expression<Func<TEntity, bool>> filter = null,
+        Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+        string includeProperties = "");
+        TEntity GetByID(object id);
+       void Insert(TEntity entity);
+        void Update(TEntity entityToUpdate);
+    }
+}
---
+++ CEDAcademyAPI/DataAccess/Repositories/EntityBase.cs
@@ -0,0 +1,77 @@
+﻿using DataAccess.IRepositories;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repositories
+{
+    public abstract class EntityBase<TEntity> : IEntityBaseRepository<TEntity> where TEntity : class
+    {
+        readonly ApplicationContext context;
+        internal DbSet<TEntity> dbSet;
+
+        public EntityBase(ApplicationContext context)
+        {
+            this.context = context;
+            this.dbSet = context.Set<TEntity>();
+        }
+
+        public virtual IEnumerable<TEntity> Get(Expression<Func<TEntity, bool>> filter = null,
+        Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+        string includeProperties = "")
+        {
+            IQueryable<TEntity> query = dbSet;
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            if (includeProperties != null)
+            {
+                foreach (var includeProperty in includeProperties.Split
+                (new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProperty);
+                }
+            }
+
+
+            if (orderBy != null)
+            {
+                return orderBy(query).ToList();
+            }
+            else
+            {
+                return query.ToList();
+            }
+        }
+
+        public virtual TEntity GetByID(object id)
+        {
+            return dbSet.Find(id);
+        }
+
+        public virtual void Insert(TEntity entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public virtual void Delete(object id)
+        {
+            TEntity entityToDelete = dbSet.Find(id);
+            Delete(entityToDelete);
+        }
+
+
+        public virtual void Update(TEntity entityToUpdate)
+        {
+            dbSet.Attach(entityToUpdate);
+            context.Entry(entityToUpdate).State = EntityState.Modified;
+        }
+    }
+}
