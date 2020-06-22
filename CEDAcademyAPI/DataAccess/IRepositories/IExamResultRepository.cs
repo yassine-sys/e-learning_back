d0562d40724f36453d600907df0e457caae529e2
yassine-sys <61196442+yassine-sys@users.noreply.github.com>
2020-06-22 16:50:49 +0200

    aa

--- CEDAcademyAPI/DataAccess/IRepositories/IExamResultRepository.cs
+++ CEDAcademyAPI/DataAccess/IRepositories/IExamResultRepository.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,12 @@ using System.Threading.Tasks;
 
 namespace DataAccess.IRepositories
 {
-    interface IExamResultRepository
+    interface IExamResultRepository<ExamResult> where ExamResult:class
     {
+        IEnumerable<ExamResult> GetExamResults();
+        ExamResult GetExamResultByID(int ResID);
+        void InsertExamResult(ExamResult examResult);
+        void DeleteExamResult(int ResID);
+        void UpdateExamResult(ExamResult examResult);
     }
 }
