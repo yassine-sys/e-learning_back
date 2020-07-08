1bc69783bb4a4ccecf58aebf241366119329e058
tesnim elfidha <tesnim.el.fidha@ced.group>
2020-07-08 16:03:45 +0100

    AutoMapper

---
+++ CEDAcademyAPI/CEDAcademyAPI/ModelsDTO/QuizResultDTO.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.ModelsDTO
+{
+  public  class QuizResultDTO
+    {
+        public int Score { get; set; }
+        public int Count { get; set; }
+        public Quiz Quiz { get; set; }
+    }
+}
